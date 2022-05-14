cf8d5e4feb9d59ebb525231b09571e834c692800
popados <popados@gmail.com>
2022-05-14 17:22:30 -0500

    added stuff

--- README.md
+++ README.md
@@ -4,10 +4,18 @@
 
 # Progress Record 2022 |
 
-### DayNum | x/x/20xx - Today
+### Day 1 | 5/11/2022 - Today
 
 
 Making monster hunter things there is an api somewhere
+
+- Got the Monster names
+
+- Got weapon names
+
+- need to change css into cards for weapons??
+
+- get 14 icons for the weapons
 
 - 
 
--- weapon-script.js
+++ weapon-script.js
@@ -1,8 +1,35 @@
 let weapon_url = "https://mhw-db.com/weapons"
 
+
+let weapon_names = []
+
+let weapon_icon_urls = []
+
+let weapon_icons = []
+
+let i = 0
+
+let weapon_types = new Set()
+
+let weapon_asset = new Set()
+
 async function apicall(url){
     return (await fetch(url)).json()
 }
+
+function seticons(icon_data) {
+
+    // weapon_asset.forEach((icon) => {if(icon != null) {weapon_icon_urls.push(icon)}})
+    const img = document.createElement("img")
+    img.src = icon_data.toString()
+    document.getElementById("icon").appendChild(img)
+
+
+}
+
+
+
+
 
 //call the weapon database
 
@@ -12,12 +39,32 @@ async function apicall(url){
 
 document.addEventListener("DOMContentLoaded", async () => { 
     weapon_data = await apicall(weapon_url)
-    console.log(weapon_data[1298])
+    // console.log(weapon_data[1298])
 
-    for (let i = 0; i < weapon_data.length; i++) {
-        console.log(weapon_data[i].name) 
+    for (i = 0; i < weapon_data.length; i++) {
+        // console.log(weapon_data[i].name)
+        weapon_names.push(weapon_data[i].name)
+        weapon_types.add(weapon_data[i].type)
+        weapon_icons.push(weapon_data[i].assets)
         
     }
-    console.log(weapon_data)
+
+    // seticons()
+
+    for (i = 0; i <= weapon_icons.length; i++) {
+        // console.log(weapon_icon_urls[i].icon)
+        if (weapon_icons[i] != null) {
+            weapon_icon_urls.push(weapon_icons[i].icon)
+
+        }
+        else if (typeof weapon_icons[i] === null) {
+            delete weapon_icons[i]
+        }
+    }
+    //displays all the icons on the page
+    //weapon_icon_urls.forEach(link => { seticons(link) })
+
+    
 
 })
+
